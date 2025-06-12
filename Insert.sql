1685cc5e08150d635ef71cc22ca473834818202c
arunkarthikeyans <57764450+arunkarthikeyans@users.noreply.github.com>
2025-06-11 20:03:12 -0600

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,6 @@
+-- insert_sample_data.sql
+INSERT INTO SAMPLE_DB.PUBLIC.CUSTOMERS (CUSTOMER_NAME, EMAIL, COUNTRY)
+VALUES 
+    ('Alice', 'alice@example.com', 'USA'),
+    ('Bob', 'bob@example.com', 'Canada'),
+    ('Charlie', 'charlie@example.com', 'UK');
