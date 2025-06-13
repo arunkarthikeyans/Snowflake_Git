f2bb280f8f4f55356ea0865843755b50fc8d9e3e
arunkarthikeyans <sarunkarthik1001@gmail.com>
2025-06-13 01:44:11 +0000

    commit

--- Insert.sql
+++ Insert.sql
@@ -3,5 +3,5 @@ INSERT INTO SAMPLE_DB.PUBLIC.CUSTOMERS (CUSTOMER_NAME, EMAIL, COUNTRY)
 VALUES 
     ('Alice', 'alice@example.com', 'USA'),
     ('Bob', 'bob@example.com', 'Canada'),
-    ('Charlie', 'charlie@example.com', 'UK');
+    ('Charlie', 'charlie@example.com', 'UK') ;
     
